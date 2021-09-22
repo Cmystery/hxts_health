6deee8311d586663522f7873f5cdf5a748c86e7b
Jie <1779758849@qq.com>
2021-09-22 16:42:50 +0800

    预约管理

---
+++ health-parent/health-interface/src/main/java/com/hxts/service/OrderSettingService.java
@@ -0,0 +1,9 @@
+package com.hxts.service;
+
+import com.hxts.pojo.OrderSetting;
+
+import java.util.List;
+
+public interface OrderSettingService {
+    void add(List<OrderSetting> orderSettingList);
+}
